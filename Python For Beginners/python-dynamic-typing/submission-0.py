89b93a1edd4d702698010e97f31083890b5bc6a9
Pranay Sood <pranay.sd@gmail.com>
2026-08-25 23:24:36 +0800

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 1
+print(type(variable))
+
+variable = 1.0
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Jammy"
+print(type(variable))
+
+variable = [1, 2, 3]
+print(type(variable))
