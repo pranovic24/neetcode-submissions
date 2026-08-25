de3a4f2d76f1f270e6516f5a1f2aa6d6ffe7df5c
Pranay Sood <pranay.sd@gmail.com>
2026-08-25 23:23:24 +0800

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 1
+float_type = 1.0
+bool_type = True
+string_type = "Jammy"
+list_type = [1, 2, 3]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
